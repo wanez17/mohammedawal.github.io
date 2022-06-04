e7fb672737a88fd7c03bf50b77a6f1a8f0551cd2
Mohammed Awal <wanm2467@gmail.com>
2022-06-04 16:57:26 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,27 @@
+
+
+
+var login = document.getElementById("login");
+
+function warnUser(){
+	alert("Sorry, the search option is currently unavailable. Kindly try the other alternatives. Thank you.");
+}
+
+
+
+//alert("Welcome")
+
+//function onClick {
+	
+/*
+let p1 = document.createElement("p");
+
+p1.textContent = "Hello World";
+
+const about = document.querySelector("#about");
+
+about.appendChild(p1);
+
+ */
+
+//}
